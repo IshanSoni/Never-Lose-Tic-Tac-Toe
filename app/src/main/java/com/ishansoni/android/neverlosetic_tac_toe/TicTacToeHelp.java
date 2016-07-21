d6079ab51b8c788e681a75a191b202916d5f725e
IshanSoni <ishansoni3@gmail.com>
2016-07-21 18:38:38 -0400

    Added tictactoe boxes

--- app/src/main/java/com/ishansoni/android/neverlosetic_tac_toe/TicTacToeHelp.java
+++ app/src/main/java/com/ishansoni/android/neverlosetic_tac_toe/TicTacToeHelp.java
@@ -1,29 +1,25 @@
 package com.ishansoni.android.neverlosetic_tac_toe;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.Button;
 
 public class TicTacToeHelp extends AppCompatActivity {
+    private Button[][] board;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tic_tac_toe_help);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        board = new Button[][] {{(Button)findViewById(R.id.button6),(Button)findViewById(R.id.button2),(Button)findViewById(R.id.button7)},
+        {(Button)findViewById(R.id.button5),(Button)findViewById(R.id.button),(Button)findViewById(R.id.button4)},
+        {(Button)findViewById(R.id.button8),(Button)findViewById(R.id.button3),(Button)findViewById(R.id.button9)}};
+
+
+
+
+
     }
-
 }
